d547d3194e54fbcd035832a5885dd8dce4451a4e
zexit <74exitz@gmail.com>
2026-03-14 13:51:40 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << unitbuf;
+  cerr << unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  cout << "$ ";
+  string text;
+  getline(cin , text);
+  cout << text << " : command not found";
+
 }
