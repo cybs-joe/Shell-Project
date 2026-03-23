9f6ab86bbc73a100ad0c757c4ffc33cffcafb9c2
zexit <74exitz@gmail.com>
2026-03-23 15:50:18 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <cstdlib>
+#include <sstream>
 using namespace std;
+namespace fs = std::filesystem ;
 
 int main() {
   // Flush after every std::cout / std:cerr
+
   string text;
   bool status = true;
   cout << unitbuf;
@@ -23,13 +28,32 @@ int main() {
     break;
    }
    else if (text == "type echo" || text =="type exit"|| text =="type type"){
-    cout <<text.substr(5,8) << " is a shell builtin" << endl;
+    cout <<text.substr(5) << " is a shell builtin" << endl;
    }
+   
    else if(text.find("type ")==0){
-    cout << text.substr(5) <<": not found" << endl;
+     const char* inv = getenv("PATH");
+     string pathstr(inv);
+     stringstream ss(inv);
+     string dir;
+     bool found = false;
+     while (getline(ss, dir, ':') ){
+      fs::path fullPath = fs:: path (dir) / text.substr(5);
+      if ( fs::exists(fullPath))
+      {
+        found = true;
+        cout << text.substr(5) << " is " << fullPath <<endl;
+        break;
+      }
+      
+     }
+     if(!found){
+      cout << text.substr(5) <<": not found" << endl;
+     }
    }
    else{
     cout << text << ": command not found" << endl;
    }
+   
   }
 }
